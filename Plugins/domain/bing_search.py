33e47d77d0ab41bec536570d700e6c16153bbed6
him <674373040@qq.com>
2024-08-09 17:45:12 +0800

    Update

--- Plugins/domain/bing_search.py
+++ Plugins/domain/bing_search.py
@@ -6,7 +6,7 @@ logging.getLogger('pyppeteer').setLevel(logging.WARNING)
 
 def get_subdomains(domain):
     search_query = f'site:{domain}'
-    num_pages = 100
+    num_pages = 30
 
     # 创建一个HTML会话
     session = HTMLSession()
@@ -19,7 +19,6 @@ def get_subdomains(domain):
             response = session.get(url)
             response.html.render(sleep=2)  # 等待页面加载
             regex_pattern = rf'https?://([a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\.{re.escape(domain)})'
-            # 提取子域名并更新集合
             subdomains.update(re.findall(regex_pattern, response.html.html))
         except Exception as e:
             print(f"请求失败: {e}")
