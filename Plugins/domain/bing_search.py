1a0205c8e1541862b2f5be10ca8c6895fc16860c
him <674373040@qq.com>
2024-08-09 17:12:01 +0800

    Update

--- Plugins/domain/bing_search.py
+++ Plugins/domain/bing_search.py
@@ -1,62 +1,30 @@
-import requests
-from bs4 import BeautifulSoup
-import time
+from requests_html import HTMLSession
 import re
-import random
+import logging
 
+logging.getLogger('pyppeteer').setLevel(logging.WARNING)
 
+def get_subdomains(domain):
+    search_query = f'site:{domain}'
+    num_pages = 100
 
-def get_subdomains(search_query):
-    base_url = "https://www.bing.com/search"
+    # 创建一个HTML会话
+    session = HTMLSession()
     subdomains = set()
-    offset = 0
-    query = f'site:{search_query}'
-    # 定义请求头
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Referer': 'https://www.bing.com/'
-    }
-
-    while True:
-        params = {
-            'q': query,
-            'first': offset + 1,
-            'count': 10
-        }
-        
+    # 遍历每一页并提取子域名
+    for page in range(num_pages):
+        start_index = page * 10
+        url = f'https://www.bing.com/search?q={search_query}&first={start_index}'
         try:
-            response = requests.get(base_url, params=params, headers=headers)
-            if response.status_code != 200:
-                print(f"请求失败，状态码: {response.status_code}")
-                break
-        except requests.RequestException as e:
-            print(f"请求异常: {e}")
-            break
-        
-        soup = BeautifulSoup(response.text, 'html.parser')
-        
-        results = soup.find_all('li', class_='b_algo')
-        if not results:
-            break
-        
-        for result in results:
-            link = result.find('a')['href']
-            # 从链接中提取子域名
-            domain_match = re.search(r'https?://([a-zA-Z0-9.-]+)', link)
-            if domain_match:
-                subdomain = domain_match.group(1)
-                if subdomain.endswith(search_query):
-                    subdomains.add(subdomain)
-        
-        offset += 10
-        print(f"当前偏移量: {offset}")
-        time.sleep(random.uniform(1, 2))
+            response = session.get(url)
+            response.html.render(sleep=2)  # 等待页面加载
+            regex_pattern = rf'https?://([a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\.{re.escape(domain)})'
+            # 提取子域名并更新集合
+            subdomains.update(re.findall(regex_pattern, response.html.html))
+        except Exception as e:
+            print(f"请求失败: {e}")
+
+    # 关闭会话
+    session.close()
 
     return list(subdomains)
-
-# 示例调用
-if __name__ == "__main__":
-    search_query = "qq.com"
-    subdomains = get_subdomains(search_query)
-    print(subdomains)
--- main.py
+++ main.py
@@ -18,11 +18,8 @@ import Plugins.domain.site_map as site_map
 import Plugins.ResultToFile.result_to_file as result_to_file
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
+
 def check_domain(domain):
-    """
-    检查域名是否存在泛解析。
-    :return: 如果不存在泛解析，返回True；否则返回False
-    """
     result = check_url.random_to_A(domain)
     if result == 5:
         print("该域名存在泛解析")
@@ -33,27 +30,32 @@ def check_domain(domain):
 
 
 def get_subdomains(domain, mode='passive'):
-    """
-    :param domain: 要检查的域名
-    :param mode: 'passive' 或 'active'，根据模式获取子域名。
-    :return: 包含各个服务结果的字典
-    """
+    results = {}
+    
+    #主线程处理
+    if mode == 'passive':
+        try:
+            bing_result = bing_search.get_subdomains(domain)
+            results['bing_search'] = bing_result
+        except Exception as e:
+            print(f"bing_search generated an exception: {e}")
+
+    # ThreadPoolExecutor处理其他插件
     with ThreadPoolExecutor() as executor:
         futures = {}
         
         if mode == 'passive':
             futures = {
-                # executor.submit(crt_sh.get_subdomains, domain): 'crt_sh',
-                # executor.submit(chaziyu_com.get_subdomains, domain): 'chaziyu_com',
-                executor.submit(bing_search.get_subdomains, domain): 'bing_search',
                 # executor.submit(google_search.get_subdomains, domain): 'google_search',
                 # executor.submit(baidu_search.get_subdomains, domain): 'baidu_search',
-                # executor.submit(dig.get_subdomains, domain): 'dig', #测试域传送
+                # executor.submit(crt_sh.get_subdomains, domain): 'crt_sh',
+                # executor.submit(chaziyu_com.get_subdomains, domain): 'chaziyu_com',
+                # executor.submit(dig.get_subdomains, domain): 'dig',
                 # executor.submit(dns_search.get_subdomains, domain): 'dns_search',
                 # executor.submit(censys_api.get_subdomains, domain): 'censys_api',
                 # executor.submit(bevigil_api.get_subdomains, domain): 'bevigil_api',
-                # executor.submit(quake.get_subdomains, domain): 'quake', #360_api目前api可以使用
-                # executor.submit(threatbook.get_subdomains, domain): 'threatbook', #微步,目前无api
+                # executor.submit(quake.get_subdomains, domain): 'quake',
+                # executor.submit(threatbook.get_subdomains, domain): 'threatbook',
             }
         elif mode == 'active':
             futures = {
@@ -65,7 +67,6 @@ def get_subdomains(domain, mode='passive'):
             print("请选择一种扫描的方式 'passive' 或 'active'")
             return {}
 
-        results = {}
         for future in as_completed(futures):
             service_name = futures[future]
             try:
@@ -74,13 +75,10 @@ def get_subdomains(domain, mode='passive'):
             except Exception as e:
                 print(f"{service_name} generated an exception: {e}")
         
-        return results
+    return results
 
 
 def main():
-    """
-    主函数，获取用户输入的域名并执行子域名获取和保存操作。
-    """
     parser = argparse.ArgumentParser(description='强大的子域收集工具')
     parser.add_argument('-active', dest='mode', action='store_const', const='active', help='选择主动扫描')
     parser.add_argument('-passive', dest='mode', action='store_const', const='passive', help='选择被动扫描')
